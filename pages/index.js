029852cab3bfee05d97cf05021e818a15b07bf3c
Bjarne Helland <bh@profitbase.no>
2020-09-08 15:35:07 +0200

    basic list

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,58 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
+const users = [
+  { name: "Bjarne Helland", email: "bh@profitbase.no" },
+  { name: "Christine Valker", email: "chv@profitbase.no" },
+  { name: "Håvard Eikeland", email: "haei@profitbase.no" },
+  { name: "Leiv Erik Larsen", email: "lel@profitbase.no" },
+  { name: "Sigve Hansen", email: "sh@profitbase.no" },
+  { name: "Tore Senneseth", email: "ts@profitbase.no" },
+  { name: "Øyvind Johnsen", email: "oej@profitbase.no" },
+];
 export default function Home() {
+  const [players, setPlayers] = useState(users);
+  const [winners, setWinners] = useState([]);
+
+  function chooseWinner() {
+    if (!players.length) return;
+
+    const winnerIndex = Math.floor(Math.random() * players.length);
+    setWinners([...winners, players[winnerIndex]]);
+    setPlayers([
+      ...players.slice(0, winnerIndex),
+      ...players.slice(winnerIndex + 1),
+    ]);
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>The winner is?</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div>
+          <h2>Players</h2>
+          <ul>
+            {players.map((user) => (
+              <li key={user.name}>{user.name}</li>
+            ))}
+          </ul>
         </div>
-      </main>
+        <div>
+          <h2>Winners</h2>
+          <ol>
+            {winners.map((user) => (
+              <li key={user.name}>{user.name}</li>
+            ))}
+          </ol>
+        </div>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+        <button onClick={chooseWinner}>Choose winner</button>
+      </main>
     </div>
-  )
+  );
 }
